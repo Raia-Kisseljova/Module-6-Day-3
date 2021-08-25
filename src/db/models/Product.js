8f92916937aa52767310e25671183913638905c8
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-08-25 15:37:22 +0100

    Created Product and Categories Models

---
+++ src/db/models/Product.js
@@ -0,0 +1,33 @@
+import sequelize from "../index.js";
+import s from "sequelize";
+const { DataTypes } = s;
+
+import Category from "./Category.js";
+
+const Product = sequelize.define(
+  "product",
+  {
+    productId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+
+      references: { model: Category, key: "categoryId" },
+    },
+  },
+  { timestamps: true }
+);
+
+export default Product;
