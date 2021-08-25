d8296d7b9bf3d829fd04648421e4ee84bf7c18db
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-08-25 15:38:24 +0100

    CategoryRoute GET,POST,DELETE,PUT works

---
+++ src/services/category.js
@@ -0,0 +1,69 @@
+import express from "express";
+
+import Category from "../db/models/Category.js";
+
+const categoryRouter = express.Router();
+
+categoryRouter
+  .route("/")
+  .get(async (req, res, next) => {
+    try {
+      const data = await Category.findAll();
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const data = await Category.create(req.body);
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+categoryRouter
+  .route("/:id")
+  .get(async (req, res, next) => {
+    try {
+      const data = await Category.findByPk(req.params.id);
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+      const data = await Category.update(req.body, {
+        where: { categoryId: req.params.id },
+        returning: true,
+      });
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const rows = await Category.destroy({
+        where: {
+          categoryId: req.params.id,
+        },
+      });
+      if (rows > 0) {
+        res.status(204).send();
+      } else {
+        res.status(404).send();
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+export default categoryRouter;
